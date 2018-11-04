ba7ab300daf6b4aacc574a77cf7c64dbb19a09f7
sefer.oezcan@stud.hs-hannover.de <Bssasukeevoo98>
2018-11-04 18:00:36 +0100

    Initial commit.

--- app/src/main/java/com/example/meistermeister/turnierplaner/MainActivity.java
+++ app/src/main/java/com/example/meistermeister/turnierplaner/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
-    private Button buttonhurenoshgn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
